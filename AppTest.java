a3f24c6ee0bb56ee0e514233f3ae88ba901ad07d
Shahzil-327 <154370038+Shahzil-327@users.noreply.github.com>
2026-01-21 03:20:41 +0500

    Add files via upload

---
+++ AppTest.java
@@ -0,0 +1,13 @@
+package com.smarttraffic;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    void sanityTest() {
+        assertTrue(true);
+    }
+}
+
